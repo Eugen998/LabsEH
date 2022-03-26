dc7cb1c8d5b8c783b268eb2fad88cdd8ed9704b5
Eugen Costin <eugen.costin@smartouch.ro>
2022-03-26 16:46:42 +0200

    Fetched patients from server

--- lab2/lab2eh/src/App.js
+++ lab2/lab2eh/src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { DataGrid } from '@mui/x-data-grid';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  const [patients, setPatients] = useState(0);
+
+  useEffect(() => {
+    getPatients()
+  }, [])
+
+  const getPatientsFromServer = async () => {
+    const URL = 'https://alexgr.ro/ehealth/patients.json'
+    const res = await fetch(URL, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+    );
+
+    return res
+  }
+
+  const getPatients = () => {
+    getPatientsFromServer().then(res => {
+      if (res.status === 200) {
+        res.json()
+          .then(res => {
+            console.log(res)
+            setPatients(res)
+          })
+      }
+    })
+  }
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 130 },
+    { field: 'first_name', headerName: 'First Name', width: 130 },
+    { field: 'last_name', headerName: 'Last Name', width: 130 },
+    { field: 'email', headerName: 'E-Mail Address', width: 250 },
+    { field: 'gender', headerName: 'Gender', width: 130 },
+  ];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: 400, width: '100%' }}>
+      {patients && <DataGrid
+        rows={patients}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />}
     </div>
   );
 }
